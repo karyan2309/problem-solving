3d656008566e409f7871818e0f117aa4a2d2b484
Aryan Kabra <52608663+karyan2309@users.noreply.github.com>
2023-07-20 08:48:50 +0530

    bubbleSort.js

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+  const swap = (arr, i, j) => ([arr[i], arr[j]] = [arr[j], arr[i]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+
+  return arr;
+}
+
+const sortedArray = bubbleSort([2, 6, 3, 8, 5, 1, 6, 4]);
+console.log(sortedArray);
